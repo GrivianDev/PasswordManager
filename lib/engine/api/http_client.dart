21a0b004991a804d4bd582b28749ba4bd6e7b92c
GrivianDev <grivian-dev@posteo.de>
2026-06-24 20:57:25 +0200

    Less verbose REST logs + app icon as plain svg

--- lib/engine/api/http_client.dart
+++ lib/engine/api/http_client.dart
@@ -4,27 +4,46 @@ import 'package:ethercrypt/engine/other/util.dart';
 import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 
+enum HttpLogLevel {
+  none,
+  basic,
+  verbose,
+}
+
 class LoggingHttpClient extends http.BaseClient {
   final http.Client _inner;
+  final HttpLogLevel logLevel;
 
-  LoggingHttpClient([http.Client? inner]) : _inner = inner ?? http.Client();
+  LoggingHttpClient([
+    http.Client? inner,
+    this.logLevel = HttpLogLevel.basic,
+  ]) : _inner = inner ?? http.Client();
 
   @override
   Future<http.StreamedResponse> send(http.BaseRequest request) async {
-    if (kDebugMode) {
-      debugPrint('---- HTTP REQUEST ----');
-      debugPrint('${request.method} ${request.url}');
-      debugPrint('Headers: ${request.headers}');
+    final stopwatch = Stopwatch()..start();
+    final response = await _inner.send(request);
+    stopwatch.stop();
+
+    if (!kDebugMode || logLevel == HttpLogLevel.none) {
+      return response;
     }
 
-    final response = await _inner.send(request);
+    if (logLevel == HttpLogLevel.basic) {
+      debugPrint(
+        '[${response.statusCode}] ${request.method} ${request.url} (${stopwatch.elapsedMilliseconds}ms)',
+      );
+      return response;
+    }
 
-    if (!kDebugMode) return response;
-
+    // verbose mode
     final responseBody = await response.stream.bytesToString();
 
-    debugPrint('---- HTTP RESPONSE ----');
-    debugPrint('Status: ${response.statusCode}');
+    debugPrint('---- HTTP REQUEST ----');
+    debugPrint('${request.method} ${request.url}');
+    debugPrint('Headers: ${request.headers}');
+    debugPrint('---- HTTP RESPONSE [${response.statusCode}] ----');
+    debugPrint('Duration: ${stopwatch.elapsedMilliseconds}ms');
     debugPrint('Headers: ${response.headers}');
     debugPrint('Body: ${prettyJson(responseBody)}');
 
